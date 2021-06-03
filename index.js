5c140e48a9fef29fb6e22346fe4a22a94a33f5a3
Jordi <jordi.colomer@philips.com>
2021-06-03 15:11:54 +0200

    trying

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 
-app.use(cors());
 app.use(express.json());
+app.use(cors());
 
 let datajson = {
   "users": [
